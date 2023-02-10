82dab55072090f264acb8108916196a0f91f17e7
FloDev23 <f.fraccastoro89@gmail.com>
2023-02-10 14:50:50 +0100

    Initial Commit

---
+++ InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by Floriano Fraccastoro on 10/02/23.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
